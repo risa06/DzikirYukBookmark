9e6d63d88305678e0b6661721b293fd37f0068d7
risa06 <risamahardika89@gmail.com>
2020-12-06 12:38:56 +0700

    first commit

---
+++ app/src/main/java/com/prakpapb1/bookmark/Bookmark2.java
@@ -0,0 +1,14 @@
+package com.prakpapb1.bookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bookmark2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark2);
+    }
+}
